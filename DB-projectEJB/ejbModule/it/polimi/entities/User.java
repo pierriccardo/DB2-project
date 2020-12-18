f79ba84b40fe81414f64ff1c98fa02249595140f
pierriccardo <pierriccardo.olivieri@mail.polimi.it>
2020-12-19 00:29:56 +0100

    entity check

---
+++ DB-projectEJB/ejbModule/it/polimi/entities/User.java
@@ -0,0 +1,100 @@
+package it.polimi.entities;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+import java.util.List;
+
+
+
+@Entity
+@Table(name = "user", schema = "DB_project")
+//@NamedQuery(name = "User.checkCredentials", query = "SELECT r FROM User r  WHERE r.username = ?1 and r.password = ?2")
+@NamedQuery(name = "User.checkUsernames", query = "SELECT r FROM User r  WHERE r.username = ?1")
+@NamedQuery(name = "User.checkEmails", query = "SELECT r FROM User r  WHERE r.email = ?1")
+@NamedQuery(name="user.RegisterUser", 
+query="INSERT INTO user (username, password, email) VALUES (?1, ?2, ?3)")
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id 	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String username;
+	private Boolean isAdmin;
+	private Boolean isBanned;
+	/*
+	@ManyToOne(mappedBy="user", fetch = FetchType.EAGER, cascade = CascadeType.REMOVE, orphanRemoval = true )
+	private List<Questionnaire> Questionnaires;
+	
+	@ManyToOne(mappedBy="user", fetch = FetchType.EAGER, cascade = CascadeType.REMOVE, orphanRemoval = true )
+	private List<Review> Reviews;
+	
+	@ManyToOne(mappedBy="user", fetch = FetchType.EAGER, cascade = CascadeType.REMOVE, orphanRemoval = true )
+	private List<Log> Logs;
+	
+	@ManyToOne(mappedBy="user", fetch = FetchType.EAGER, cascade = CascadeType.REMOVE, orphanRemoval = true )
+	private List<Leaderboard> Leaderboards;
+	*/
+	public User() {
+	}
+	
+	public Boolean getIsAdmin() {
+		return isAdmin;
+	}
+
+	public void setIsAdmin(Boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+
+	public Boolean getIsBanned() {
+		return isBanned;
+	}
+
+	public void setIsBanned(Boolean isBanned) {
+		this.isBanned = isBanned;
+	}
+	/*
+	public List<Quesitonnaire> getQuestionnaires() {
+		return Questionnaires;
+	}
+
+	public void setQuestionnaires(List<Quesitonnaire> questionnaires) {
+		Questionnaires = questionnaires;
+	}
+
+	public List<Review> getReviews() {
+		return Reviews;
+	}
+
+	public void setReviews(List<Review> reviews) {
+		Reviews = reviews;
+	}
+
+	public List<Log> getLogs() {
+		return Logs;
+	}
+
+	public void setLogs(List<Log> logs) {
+		Logs = logs;
+	}
+
+	public List<Leaderboard> getLeaderboards() {
+		return Leaderboards;
+	}
+
+	public void setLeaderboards(List<Leaderboard> leaderboards) {
+		Leaderboards = leaderboards;
+	}
+	*/
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+}
