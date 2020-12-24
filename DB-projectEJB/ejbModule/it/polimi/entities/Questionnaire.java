70ca2431a3615ab0d16738ee03c2469d2065c4a8
pierriccardo <pierriccardo.olivieri@mail.polimi.it>
2020-12-24 23:28:23 +0100

    small fixes

--- DB-projectEJB/ejbModule/it/polimi/entities/Questionnaire.java
+++ DB-projectEJB/ejbModule/it/polimi/entities/Questionnaire.java
@@ -1,49 +1,31 @@
 package it.polimi.entities;
 
 import java.io.Serializable;
-<<<<<<< HEAD
 import javax.persistence.*;
 import java.util.List;
-=======
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQuery;
-import javax.persistence.Table;
->>>>>>> 6fadb62987b19f619b02d181c2444cda0eda7dc0
+import javax.persistence.Table;	
 
 
 @Entity
 @Table(name = "questionnaire", schema = "DB_project")
-<<<<<<< HEAD
-//@NamedQuery(name = "Questionnaire.check", query = "SELECT * FROM Questionnaire")
-
-=======
 @NamedQuery(name = "Questionnaire.getUserQuestionnaires", query = "SELECT q FROM Questionnaire q  WHERE q.id = ?1")
 @NamedQuery(name="Questionnaire.insertUserQuestionnaire", query="INSERT INTO Questionnaire (idProduct, idUser, sex, age, expertise_level) VALUES (?1, ?2, ?3, ?4, ?5)")
->>>>>>> 6fadb62987b19f619b02d181c2444cda0eda7dc0
 public class Questionnaire implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id 	
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-<<<<<<< HEAD
-
-=======
-	
->>>>>>> 6fadb62987b19f619b02d181c2444cda0eda7dc0
 	private int idProduct;
 	private int idUser;
 	private int sex;
 	private int age;
 	private int expertise_level;
-<<<<<<< HEAD
-
-}
-=======
 	
 	public Questionnaire() {
 	}
@@ -97,4 +79,3 @@ public class Questionnaire implements Serializable {
 	}
 
 }
->>>>>>> 6fadb62987b19f619b02d181c2444cda0eda7dc0
