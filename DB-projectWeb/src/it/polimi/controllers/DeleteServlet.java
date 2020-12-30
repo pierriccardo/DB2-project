95e2161a8b48a15b2e8ae8d8e1bcba588aa2eca1
gianmtedeschi <ian@ian-VirtualBox>
2020-12-30 23:59:53 +0100

    aggiornata delete + page html

--- DB-projectWeb/src/it/polimi/controllers/DeleteServlet.java
+++ DB-projectWeb/src/it/polimi/controllers/DeleteServlet.java
@@ -3,11 +3,17 @@ package it.polimi.controllers;
 import java.io.IOException;
 
 import javax.ejb.EJB;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 import it.polimi.services.QuestionnaireService;
 
@@ -17,23 +23,41 @@ public class DeleteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	@EJB(name = "it.polimi.services/QuestionnaireService")
 	private QuestionnaireService qService;
+	private TemplateEngine templateEngine;
+
 
 	public DeleteServlet() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	public void init() throws ServletException {
+		ServletContext servletContext = getServletContext();
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		this.templateEngine = new TemplateEngine();
+		this.templateEngine.setTemplateResolver(templateResolver);
+		templateResolver.setSuffix(".html");
+	}
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		String path = "/WEB-INF/AdminDeleteQuestionnaire.html";
+		ServletContext servletContext = getServletContext();
+		final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+		templateEngine.process(path, ctx, response.getWriter());
+
 		Integer qId = null;
 		try {
 			qId = Integer.parseInt(request.getParameter("qId"));
 		} catch (Exception e) {
-			response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid Questionnaire parameters");
+			ctx.setVariable("errorMsg", "Invalid Questionnaire parameters");
+			path = "/WEB-INF/AdminDeleteQuestionnaire.html";
+			templateEngine.process(path, ctx, response.getWriter());
 		}
 		qService.deleteAlbum(qId);
 		String ctxpath = getServletContext().getContextPath();
-		String path = ctxpath + "/GoToHomePage";
+		path = ctxpath + "/GoToHomeAdminPage";
 		response.sendRedirect(path);
 	}
 
