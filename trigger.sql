1aa48b30313a214ca1aed97c9e618cfdd6fa4e2f
lipidex <contact@pierpaoloagamennone.com>
2021-02-24 15:14:00 +0100

    Some fix

--- trigger.sql
+++ trigger.sql
@@ -5,16 +5,15 @@ CREATE TRIGGER point1
 AFTER INSERT ON answer
 FOR EACH ROW 
 BEGIN
-    DECLARE x int;
+    DECLARE my_idUser int;
     
-    SELECT idUser
-    INTO x
+    SELECT idUser INTO my_idUser
     FROM questionnaire 
-    WHERE new.idQuestionnaire = id ;
+    WHERE new.idQuestionnaire = id;
 
     UPDATE user
     SET score = score + 1 
-    WHERE id = x;
+    WHERE id = my_idUser;
 END;$$
 
 delimiter ; 
@@ -27,16 +26,15 @@ CREATE TRIGGER point1Remove
 AFTER DELETE ON answer
 FOR EACH ROW 
 BEGIN
-    DECLARE x int;
+    DECLARE my_idUser int;
     
-    SELECT idUser
-    INTO x
+    SELECT idUser INTO my_idUser
     FROM questionnaire 
     WHERE old.idQuestionnaire = id ;
 
     UPDATE user
     SET score = score - 1 
-    WHERE id = x;
+    WHERE id = my_idUser;
 END;$$
 
 delimiter ; 
@@ -49,21 +47,21 @@ CREATE TRIGGER point2
 AFTER INSERT ON questionnaire 
 FOR EACH ROW 
 BEGIN
-    IF(new.sex IS NOT NULL) then
+    IF (new.sex IS NOT NULL) THEN
         UPDATE user
         SET score = score + 2
         WHERE id = new.idUser;
-    end if;
-    IF(new.age IS NOT NULL) then
+    END IF;
+    IF (new.age IS NOT NULL) THEN
         UPDATE user
         SET score = score + 2
         WHERE id = new.idUser;
-    end if;
-    IF(new.expertise_level IS NOT NULL) then
+    END IF;
+    IF (new.expertise_level IS NOT NULL) THEN
         UPDATE user
         SET score = score + 2
         WHERE id = new.idUser;
-    end if;
+    END IF;
 END;$$
 
 delimiter ; 
@@ -76,25 +74,25 @@ CREATE TRIGGER point2Remove
 AFTER DELETE ON questionnaire 
 FOR EACH ROW 
 BEGIN
-    IF(old.sex IS NOT NULL) then
+    IF (old.sex IS NOT NULL) THEN
         UPDATE user
         SET score = score - 2
         WHERE id = old.idUser;
-    end if;
-    IF(old.age IS NOT NULL) then
+    END IF;
+    IF (old.age IS NOT NULL) THEN
         UPDATE user
         SET score = score - 2
         WHERE id = old.idUser;
-    end if;
-    IF(old.expertise_level IS NOT NULL) then
+    END IF;
+    IF (old.expertise_level IS NOT NULL) THEN
         UPDATE user
         SET score = score - 2
         WHERE id = old.idUser;
-    end if;
-
+    END IF;
 END;$$
 
 delimiter ; 
+
 
 /*BANNED WORD*/
 delimiter $$
@@ -103,17 +101,13 @@ CREATE TRIGGER bannedWord
 BEFORE INSERT ON answer 
 FOR EACH ROW
 BEGIN
-if (SELECT count(*)
-                FROM blacklist_word
-                WHERE new.text LIKE CONCAT('%', word, '%')) > 0 then
-
-
-    DELETE questionnaire
-    WHERE new.idQuestionnaire = id;
-
-    SIGNAL sqlstate '45001' set message_text = "User used a banned word!";
-    
-    end if;
+	IF ((SELECT count(*) 
+		FROM blacklist_word 
+		WHERE new.text LIKE CONCAT('%', word, '%')) > 0) 
+	THEN
+		DELETE FROM questionnaire WHERE new.idQuestionnaire = id;
+		SIGNAL sqlstate '45001' set message_text = "User used a banned word!";
+	END IF;
 END;$$
 
 delimiter ; 
@@ -126,13 +120,13 @@ CREATE TRIGGER noMoreThanTwo
 BEFORE INSERT ON product 
 FOR EACH ROW
 BEGIN
-if (SELECT count(*)
-                FROM product
-                WHERE new.date = date) then
-
-    SIGNAL sqlstate '45002' set message_text = "There can be only one product for a day!";
-    
-    end if;
+	IF (SELECT count(*)
+		FROM product
+		WHERE new.date = date) 
+		
+	THEN
+		SIGNAL sqlstate '45002' set message_text = "There can be only one product for a day!";
+	END IF;
 END;$$
 
-delimiter ; 
+delimiter ;
