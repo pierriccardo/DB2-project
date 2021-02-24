9573359fb7f10b9a7e7e6110acba6f75c8780cab
lipidex <contact@pierpaoloagamennone.com>
2021-02-24 22:20:51 +0100

    New version trigger

--- triggers.sql
+++ triggers.sql
@@ -5,38 +5,15 @@ CREATE TRIGGER point1
 AFTER INSERT ON answer
 FOR EACH ROW 
 BEGIN
-    DECLARE x int;
-    
-    SELECT idUser
-    INTO x
-    FROM questionnaire 
-    WHERE new.idQuestionnaire = id AND new.isSubmitted = 1;
+	DECLARE my_idUser INT;
 
-    UPDATE user
-    SET score = score + 1 
-    WHERE id = x;
-END;$$
+	SELECT idUser INTO my_idUser
+	FROM questionnaire 
+	WHERE new.idQuestionnaire = id AND new.isSubmitted = 1;
 
-delimiter ; 
-
-
-/*ONE POINT FOR EACH ANSWER OF SECTION 1*/
-delimiter $$
-
-CREATE TRIGGER point1Remove
-AFTER DELETE ON answer
-FOR EACH ROW 
-BEGIN
-    DECLARE x int;
-    
-    SELECT idUser
-    INTO x
-    FROM questionnaire 
-    WHERE old.idQuestionnaire = id AND old.isSubmitted = 1;
-
-    UPDATE user
-    SET score = score - 1 
-    WHERE id = x;
+	UPDATE user
+	SET score = score + 1 
+	WHERE id = my_idUser;
 END;$$
 
 delimiter ; 
@@ -49,52 +26,77 @@ CREATE TRIGGER point2
 AFTER INSERT ON questionnaire 
 FOR EACH ROW 
 BEGIN
-    IF(new.sex > 0 AND new.isSubmitted = 1) then
-        UPDATE user
-        SET score = score + 2
-        WHERE id = new.idUser;
-    end if;
-    IF(new.age > 0 AND new.isSubmitted = 1) then
-        UPDATE user
-        SET score = score + 2
-        WHERE id = new.idUser;
-    end if;
-    IF(new.expertise_level > 0 AND new.isSubmitted = 1) then
-        UPDATE user
-        SET score = score + 2
-        WHERE id = new.idUser;
-    end if;
+	IF (new.sex > 0 AND new.isSubmitted = 1) THEN
+		UPDATE user
+		SET score = score + 2
+		WHERE id = new.idUser;
+	END IF;
+	IF (new.age > 0 AND new.isSubmitted = 1) THEN
+		UPDATE user
+		SET score = score + 2
+		WHERE id = new.idUser;
+	END IF;
+	IF (new.expertise_level > 0 AND new.isSubmitted = 1) THEN
+		UPDATE user
+		SET score = score + 2
+		WHERE id = new.idUser;
+    END IF;
 END;$$
 
 delimiter ; 
 
 
-/*ONE POINT FOR EACH ANSWER OF SECTION 2*/
+/*REMOVE QUESTIONNAIRES OF A PRODUCT*/
 delimiter $$
 
-CREATE TRIGGER point2Remove
-AFTER DELETE ON questionnaire 
+CREATE TRIGGER removeQuestionnaireProduct
+BEFORE DELETE ON product
 FOR EACH ROW 
 BEGIN
-    IF(old.sex > 0 AND old.isSubmitted = 1) then
-        UPDATE user
-        SET score = score - 2
-        WHERE id = old.idUser;
-    end if;
-    IF(old.age > 0 AND old.isSubmitted = 1) then
-        UPDATE user
-        SET score = score - 2
-        WHERE id = old.idUser;
-    end if;
-    IF(old.expertise_level > 0 AND old.isSubmitted = 1) then
-        UPDATE user
-        SET score = score - 2
-        WHERE id = old.idUser;
-    end if;
-
+	DELETE FROM questionnaire WHERE idProduct = old.id;
 END;$$
 
 delimiter ; 
+
+
+/*REMOVE POINTS OF DELETED QUESTIONNAIRES*/
+delimiter $$
+
+CREATE TRIGGER removePoints
+BEFORE DELETE ON questionnaire
+FOR EACH ROW 
+BEGIN
+    DECLARE my_score INT;
+    
+    SELECT count(*) INTO my_score FROM answer WHERE idQuestionnaire = old.id;
+    
+    IF (old.sex > 0 AND old.isSubmitted = 1) THEN
+        /* UPDATE user
+        SET score = score - 2
+        WHERE id = old.idUser; */
+        
+        SELECT my_score + 2 INTO my_score;
+    END IF;
+    IF (old.age > 0 AND old.isSubmitted = 1) THEN
+        /* UPDATE user
+        SET score = score - 2
+        WHERE id = old.idUser; */
+        
+        SELECT my_score + 2 INTO my_score;
+    END IF;
+    IF (old.expertise_level > 0 AND old.isSubmitted = 1) THEN
+        /*UPDATE user
+        SET score = score - 2
+        WHERE id = old.idUser; */
+        
+        SELECT my_score + 2 INTO my_score;
+    END IF;
+    
+    UPDATE user SET score = score - my_score WHERE id = old.idUser;
+END;$$
+
+delimiter ; 
+
 
 /*BANNED WORD*/
 delimiter $$
@@ -103,17 +105,13 @@ CREATE TRIGGER bannedWord
 BEFORE INSERT ON answer 
 FOR EACH ROW
 BEGIN
-if (SELECT count(*)
-                FROM blacklist_word
-                WHERE new.text LIKE CONCAT('%', word, '%')) > 0 then
-
-
-    DELETE FROM questionnaire
-    WHERE new.idQuestionnaire = id;
-
-    SIGNAL sqlstate '45001' set message_text = "User used a banned word!";
-    
-    end if;
+	IF ((SELECT count(*)
+			FROM blacklist_word
+			WHERE new.text LIKE CONCAT('%', word, '%')) > 0)
+	THEN
+		DELETE FROM questionnaire WHERE new.idQuestionnaire = id;
+        SIGNAL sqlstate '45001' set message_text = "User used a banned word!";
+    END IF;
 END;$$
 
 delimiter ; 
@@ -126,22 +124,25 @@ CREATE TRIGGER noMoreThanTwo
 BEFORE INSERT ON product 
 FOR EACH ROW
 BEGIN
-if (SELECT count(*)
-                FROM product
-                WHERE new.date = date) then
-
-    SIGNAL sqlstate '45001' set message_text = "There can be only one product for a day!";
-    
-    end if;
+	IF (SELECT count(*) 
+			FROM product
+			WHERE new.date = date)
+	THEN
+		SIGNAL sqlstate '45001' set message_text = "There can be only one product for a day!";
+    END IF;
 END;$$
 
 delimiter ; 
 
+
+
+/*
 show triggers;
 
 DROP TRIGGER point1;
-DROP TRIGGER point1Remove;
-DROP TRIGGER noMoreThanTwo;
-DROP TRIGGER bannedWord;
-DROP TRIGGER point2Remove;
 DROP TRIGGER point2;
+DROP TRIGGER removeQuestionnaireProduct;
+DROP TRIGGER removePoints;
+DROP TRIGGER bannedWord;
+DROP TRIGGER noMoreThanTwo;
+*/
